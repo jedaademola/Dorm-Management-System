031490c4647533ec01556879afc0c440c3c8b2e3
Ferdis17 <fmuriyesu@users.noreply.github.com>
2017-12-15 15:21:19 -0600

    Merge branch 'Kibre-dev' of https://github.com/jedaademola/dormmanagementsystem into ferdis-dev
    ok

--- src/main/java/edu/mum/cs544/model/RoomApplication.java
+++ src/main/java/edu/mum/cs544/model/RoomApplication.java
@@ -1,5 +1,7 @@
 package edu.mum.cs544.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.OneToOne;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,5 +11,42 @@ public class RoomApplication implements Serializable {
 
     private Date applicationDate;
     private Date arrivingDate;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Building buildingPreference;
+
+    public RoomApplication() {
+    }
+
+    public long getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(long applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public Date getApplicationDate() {
+        return applicationDate;
+    }
+
+    public void setApplicationDate(Date applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+
+    public Date getArrivingDate() {
+        return arrivingDate;
+    }
+
+    public void setArrivingDate(Date arrivingDate) {
+        this.arrivingDate = arrivingDate;
+    }
+
+    public Building getBuildingPreference() {
+        return buildingPreference;
+    }
+
+    public void setBuildingPreference(Building buildingPreference) {
+        this.buildingPreference = buildingPreference;
+    }
 }
