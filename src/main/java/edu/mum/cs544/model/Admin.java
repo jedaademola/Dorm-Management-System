b9f84ff9fe753f894e4fec3dac1ae15ff7645551
KibrewosenData <kdemeke5@gmail.com>
2017-12-15 14:51:19 -0600

    Added model classes+ Refactord Entity relationships

--- src/main/java/edu/mum/cs544/model/Admin.java
+++ src/main/java/edu/mum/cs544/model/Admin.java
@@ -2,10 +2,24 @@ package edu.mum.cs544.model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Admin extends Person {
     private String staffId;
+    @OneToMany
+    @JoinColumn(name = "Student_id")
+    private List<Student> students = new ArrayList<Student>();
+    @OneToMany
+    @JoinColumn(name = "building_id")
+    private List<Building> buildings = new ArrayList<Building>();
+    @OneToMany
+    @JoinColumn(name = "room_id")
+    private List<Room> rooms = new ArrayList<Room>();
+
 
     //Constructor
     public Admin() {
@@ -19,5 +33,29 @@ public class Admin extends Person {
 
     public void setStaffId(String staffId) {
         this.staffId = staffId;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Building> getBuildings() {
+        return buildings;
+    }
+
+    public void setBuildings(List<Building> buildings) {
+        this.buildings = buildings;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
     }
 }
--- src/main/java/edu/mum/cs544/model/Room.java
+++ src/main/java/edu/mum/cs544/model/Room.java
@@ -1,9 +1,8 @@
 package edu.mum.cs544.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Room {
@@ -12,5 +11,45 @@ public class Room {
     @GeneratedValue
     private long id;
     private String roomNo;
+    @OneToMany(mappedBy = "room")
+    private List<Student> students;
+    @ManyToOne
+    private Building building;
 
+    //Constructor
+    public Room() {
+    }
+    //Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(String roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
 }
