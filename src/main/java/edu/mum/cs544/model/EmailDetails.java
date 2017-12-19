e5f2133ebd21d79a16a1e7ac4b5ab1aa29308ce8
Lukman Arogundade <jedaademola@users.noreply.github.com>
2017-12-19 09:40:10 -0600

    updates

---
+++ src/main/java/edu/mum/cs544/model/EmailDetails.java
@@ -0,0 +1,86 @@
+package edu.mum.cs544.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class EmailDetails {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String sender;
+    private String recipient;
+    private String subject;
+    private String contentType;
+    private int status;
+    private Timestamp createdOn;
+    private String content;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Timestamp createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/edu/mum/cs544/service/EmailService.java
@@ -0,0 +1,81 @@
+package edu.mum.cs544.service;
+
+
+import edu.mum.cs544.dao.AbstractDao;
+import edu.mum.cs544.dao.EmailDao;
+import edu.mum.cs544.model.AbstractModel;
+import edu.mum.cs544.model.EmailDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+
+@Service
+public class EmailService extends AbstractService<AbstractModel> {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    @Autowired
+    public EmailService(@Qualifier("emailDao") AbstractDao<AbstractModel> dao){//,JavaMailSender javaMailSender) {
+        super(dao);
+       // this.javaMailSender = javaMailSender;
+    }
+
+    public void queueEmail(EmailDetails email) {
+
+        EmailDao emailDao = (EmailDao) this.dao;
+        emailDao.queueMessage(email);
+    }
+
+    public void sendQueuedEmails() {
+        EmailDao emailDao = (EmailDao) this.dao;
+        try {
+
+            List<EmailDetails> emailList = emailDao.getEmailsToSend();
+
+        for(EmailDetails e : emailList) {
+
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setTo(e .getRecipient());
+            message.setSubject(e .getSubject());
+            message.setText(e.getContent());
+            message.setFrom(e.getSender());
+
+            javaMailSender.send(message);
+
+            updateEmailStatus(e.getId(),1);
+        }
+
+        }catch (Exception e)
+        {
+            logger.error("sendQueuedEmails(): " + e.getMessage());
+        }
+
+    }
+
+    public void updateEmailStatus(long id, int status) {
+
+        EmailDao emailDao = (EmailDao) this.dao;
+        emailDao.updateEmailStatus(id, status);
+    }
+}
